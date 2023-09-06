21461aa7d628cd7b396712c9dfc8850a89141ab7
Jiun Sull <jsull2025@chadwickschool.org>
2023-09-06 11:40:03 -0700

    Completed all 4 Sorts

--- Sorting.java
+++ Sorting.java
@@ -27,7 +27,7 @@ public class Sorting
      * @param  y  a sample parameter for a method
      * @return    the sum of x and y
      */
-    public void bubbleSort(int[] arr)
+    public static void bubbleSort(int[] arr)
     {
         // put your code here
         for (int i = 0; i < arr.length - 1; i++) {
@@ -44,7 +44,7 @@ public class Sorting
         }
     }
     
-    public void selectionSort(int[] arr) {
+    public static void selectionSort(int[] arr) {
         
         for(int i = 0; i < arr.length - 1; i++) {
             int minLocation = i;
@@ -59,26 +59,65 @@ public class Sorting
         }
         
     }
-    public void merge(int[] arr, int beg, int end, int mid) {
+    
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int cur = i - 1;
+            
+            while (cur > -1 && temp < arr[cur]) {
+                arr[cur+1] = arr[cur];
+                cur--;
+            }
+            arr[cur+1] = temp;
+            
+        }
+    }
+    
+    public static void merge(int[] arr, int beg, int end, int mid) {
         
+        int i = 0;
         int left = beg;
         int right = mid + 1;
-        int[] arr2 = new int[end+1]
+        int[] arr2 = new int[end-beg+1];
         
-        while (left <= mid || right <= end) {
+        while (left <= mid && right <= end) {
             if (arr[left] < arr[right]) {
-                arr[beg] == arr[left]
+                arr2[i] = arr[left];
+                left++;
+                i++;
+            } else {
+                arr2[i] = arr[right];
+                right++;
+                i++;
             }
+        }
+        
+        while (left <= mid) {
+            arr2[i] = arr[left];
+            left++;
+            i++;
+        }
+        
+        while (right <= end) {
+            arr2[i] = arr[right];
+            right++;
+            i++;
+        }
+        
+        for (int j = 0; j < arr2.length; j++) {
+            arr[j+beg] = arr2[j];
+            
         }
         
     }
     
-    public void mergeSort(int[] arr) {
+    public static void mergeSort(int[] arr) {
         mergeSort(0, arr.length - 1, arr);
     
     }
     
-    public void mergeSort(int beg, int end, int[] arr) {
+    public static void mergeSort(int beg, int end, int[] arr) {
         if (beg == end) {
             return;
         } else {
@@ -93,14 +132,14 @@ public class Sorting
     }
     
     public void testSorting() {
-        int[] arr1 = {23, 10, 55, 73, 62};
+        int[] arr1 = {23, 73, 55, 10, 15, 62};
         int[] arr2 = {1, 2, 3, 4, 5};
         int[] arr3 = {-4, 2, 0, 4, 1};
         int[] arr4 = {2, 2, 0, 4, 10};
-        mergeSort(arr1);
-        mergeSort(arr2);
-        mergeSort(arr3);
-        mergeSort(arr4);
+        insertionSort(arr1);
+        insertionSort(arr2);
+        insertionSort(arr3);
+        insertionSort(arr4);
         System.out.println(Arrays.toString(arr1));
         System.out.println(Arrays.toString(arr2));
         System.out.println(Arrays.toString(arr3));
